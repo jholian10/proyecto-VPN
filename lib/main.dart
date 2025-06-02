604241b715d8f07c9e4b8cd6ef04604232c2c7f5
Jholian Mannnuel <jholianmanuel10@gmail.com>
2025-06-02 12:47:55 -0500

    diseñado mi primera vista

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class MainProgram extends StatelessWidget {
+  const MainProgram({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material();
+  }
+}
